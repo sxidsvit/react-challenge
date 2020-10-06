d7e47bb7cd427a9ee8245755c262fda2cd68a48e
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-06 06:59:31 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -2,22 +2,144 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import asabenehImage from './images/asabeneh.jpg'
+import html5Logo from './images/html_logo.png'
+import css3Logo from './images/css_logo.png'
+import jsLogo from './images/js_logo.png'
+import reactLogo from './images/react_logo.png'
 
 
-// To get the root element from the HTML document
-const rootElement = document.querySelector('#root')
-// JSX element, header
-const welcome = 'Welcome to 30 Days Of React Challenge'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
+const styles = {
+  imgTech: {
+    height: "200px",
+    width: "auto"
+  },
+  tech: {
+    backgroundColor: "#F0F1F7",
+    padding: "2rem",
+    margine: "1rem"
+  },
+  userCardBorder: {
+    border: "1px solid #2ACFCF",
+    borderRadius: "20px",
+    padding: "1rem",
+    margin: "1rem"
+  },
+  UserCardMark: {
+    width: "10px",
+    height: "10px",
+    padding: "6px",
+    borderRadius: "50%",
+    backgroundColor: "#2ACFCF",
+    color: "#fff"
+  },
+  userCardImg: {
+    height: "150px",
+    width: "auto",
+    borderRadius: "50%",
+    marginBottom: "0.5rem"
+  },
+  userSkill: {
+    borderRadius: "5px",
+    marginRight: "10px",
+    marginBottom: "10px",
+    border: "1px solid #2ACFCF",
+    backgroundColor: "#2ACFCF",
+    color: "#fff",
+    padding: "5px"
+  },
+  subscribeBorder: {
+    border: "1px solid #C2E6F4",
+    backgroundColor: "#C2E6F4",
+    borderRadius: "20px",
+    padding: "1rem",
+    margin: "1rem"
+  },
+  subscribeH1: {
+    fontSize: "2rem",
+    fontWeight: "300",
+    letterSpacing: "0px"
+  },
+  subscribeButton: {
+    backgroundColor: "#F37474",
+    color: "#fff",
+    padding: "0.5rem",
+    width: "16rem",
+    borderStyle: "none",
+    borderRadius: "4px",
+    borderColor: "#F37474",
+    fontWeight: "300",
+    marginTop: "1rem"
+  },
+  subscribeInput: {
+    margin: "1rem",
+    width: "30%",
+    borderColor: "#C2E6F4",
+    borderStyle: "none",
+    borderRadius: "4px",
+    padding: "0.5rem 2rem"
+  }
+}
+
+const subscribe = (
+  <div className="d-flex flex-column justify-content-center align-items-center"
+    style={styles.subscribeBorder}>
+    <h1 style={styles.subscribeH1}>SUBSCRIBE</h1>
+    <p>Sign up with your email address to receive news and updates</p>
+    <div className="d-flex justify-content-around" >
+      <input style={styles.subscribeInput} type="text" placeholder="First name" />
+      <input style={styles.subscribeInput} type="text" placeholder="Lirst name" />
+      <input style={styles.subscribeInput} type="text" placeholder="Email" />
+    </div>
+    <button style={styles.subscribeButton}>Subscribe</button>
+  </div>
+)
+
+const frontEndTechnologies = (
+  <div style={styles.tech} className="d-flex flex-column justify-content-center align-items-center">
+    <h6 className="text-center  mb-4 font-weight-bold">Front End Technologies</h6>
+    <div div style={{ display: "flex", flexDirection: "row" }}>
+      <img style={styles.imgTech} src={html5Logo} alt="html5Logo" />
+      <img style={styles.imgTech} src={css3Logo} alt="css3Logo" />
+      <img style={styles.imgTech} src={jsLogo} alt="css3Logo" />
+      <img style={styles.imgTech} src={reactLogo} alt="reactLogo" />
+    </div>
+  </div>
+)
+
 const author = {
   firstName: 'Asabeneh',
   lastName: 'Yetayeh',
 }
+const techs = ['HTML', 'CSS', 'JavaScript', 'Sass', 'JS', 'React', 'Redux', 'Node', 'MongoDB', 'Python', 'Flask', 'Django', 'MySQL', 'GraphQL', 'Docker', 'Heroku', 'Git']
+const techsFormatted = techs.map((tech, i) =>
+  <span key={i} style={styles.userSkill} >{tech}</span>)
+
+const userCard = (
+  <div style={styles.userCardBorder}>
+    <img style={styles.userCardImg} src={asabenehImage} alt="html5Logo" />
+    <p><strong>{author.firstName}&nbsp;{author.lastName}</strong>&nbsp;
+      <strong style={styles.UserCardMark}>&#10004;</strong>
+    </p>
+    <p>Senior Developer, Finland</p>
+    <div className="d-flex flex-wrap">
+      {techsFormatted}
+    </div>
+    <p>&#8986;&nbsp;Joined on Aug 30, 2020</p>
+  </div>
+)
+
+
+
+
+// JSX element, header
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+
 const date = 'Oct 2, 2020'
 
 // JSX element, header
-const Header = () => (
+const header = (
   <header>
     <div className='header-wrapper'>
       <h1>{welcome}</h1>
@@ -31,96 +153,52 @@ const Header = () => (
   </header>
 )
 
-const numOne = 3
-const numTwo = 2
-
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
-)
-
-const yearBorn = 1820
-const currentYear = 2020
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-)
-
-// User Card Component
-const UserCard = () => (
-  <div className='user-card'>
-    <img src={asabenehImage} alt='asabeneh image' />
-    <h2>
-      {author.firstName} {author.lastName}
-    </h2>
-  </div>
-)
-
 // JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
-
-// JSX element, main
-const Main = () => (
+const main = (
   <main>
     <div className='main-wrapper'>
-      <div>
-        <p>
-          Prerequisite to get started{' '}
-          <strong>
-            <em>react.js</em>
-          </strong>
-          :
-        </p>
-        <ul>{techsFormatted}</ul>
-        {result}
-        {personAge}
-      </div>
-      <UserCard />
+      {frontEndTechnologies}
+      {userCard}
+      {subscribe}
     </div>
   </main>
 )
 
-const copyRight = '2020'
+const copyRight = 'Copyright 2020'
 
 // JSX element, footer
-const Footer = () => (
+const footer = (
   <footer>
     <div className='footer-wrapper'>
-      <p>Copyright &copy;{copyRight}</p>
+      <p>{copyRight}</p>
     </div>
   </footer>
 )
 
 // JSX element, app
-const App = () => (
+const app = (
   <div className='app'>
-    <Header />
-    <Main />
-    <Footer />
+    {header}
+    {main}
+    {footer}
   </div>
 )
 
-// we render the App component using the ReactDOM package
-// ReactDOM.render(<App />, rootElement)
-
-
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(app, rootElement)
 //  =================================================================
 
 // Hexadecimal color generator
-const hexaColor = () => {
-  let str = '0123456789abcdef'
-  let color = ''
-  for (let i = 0; i < 6; i++) {
-    let index = Math.floor(Math.random() * str.length)
-    color += str[index]
-  }
-  return '#' + color
-}
+// const hexaColor = () => {
+//   let str = '0123456789abcdef'
+//   let color = ''
+//   for (let i = 0; i < 6; i++) {
+//     let index = Math.floor(Math.random() * str.length)
+//     color += str[index]
+//   }
+//   return '#' + color
+// }
 
-const HexaColor = () => <div>{hexaColor()}</div>
-ReactDOM.render(<HexaColor />, rootElement)
+// const HexaColor = () => <div>{hexaColor()}</div>
+// ReactDOM.render(<HexaColor />, rootElement)
