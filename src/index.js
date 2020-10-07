0a77c1299b49159fcf7124370b14d57f20ec9b3d
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-07 21:44:36 +0300

    Exercises: N1 Level 2

--- src/index.js
+++ src/index.js
@@ -1,111 +1,62 @@
-
-// ==== Mapping array of arrays ====
-
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
-// const skills = [
-//   ['HTML', 10],
-//   ['CSS', 7],
-//   ['JavaScript', 9],
-//   ['React', 8],
-// ]
-
-// // Skill Component
-// const Skill = ({ skill: [tech, level] }) => (
-//   <li>
-//     {tech} {level}
-//   </li>
-// )
-
-// // Skills Component
-// const Skills = ({ skills }) => {
-//   const skillsList = skills.map((skill) => <Skill skill={skill} />)
-//   console.log(skillsList)
-//   return <ul>{skillsList}</ul>
-// }
-
-// const App = () => {
-//   return (
-//     <div className='container'>
-//       <div>
-//         <h1>Skills Level</h1>
-//         <Skills skills={skills} />
-//       </div>
-//     </div>
-//   )
-// }
-
-// const rootElement = document.getElementById('root')
-// ReactDOM.render(<App />, rootElement)
-
-// ==== Mapping array of objects ====
-
+// index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const countries = [
-  { name: 'Finland', city: 'Helsinki' },
-  { name: 'Sweden', city: 'Stockholm' },
-  { name: 'Denmark', city: 'Copenhagen' },
-  { name: 'Norway', city: 'Oslo' },
-  { name: 'Iceland', city: 'Reykjavík' },
-]
+const Square = ({ color, number }) => {
+  console.log('Square color: ', color);
+  const styles = {
+    square: {
+      width: "80px",
+      height: "80px",
+      background: color,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+      border: "2px solid #fff",
+      color: "#fff",
+      fontWeight: "500",
+      fontSize: "24px",
+    }
+  }
 
-// Country component
-const Country = ({ country: { name, city } }) => {
   return (
-    <div>
-      <h5>{name}</h5>
-      <small>{city}</small>
+    <div style={styles.square} > { number}</div>
+  )
+}
+
+// JSX element App
+const App = () => {
+  const colors = ["#21BF73", "#FDDB3A", "#FD5E53"]
+  const squareList = []
+
+  const numberColor = (number, colors) => {
+    const primeNumber = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+    let color = "#fff"
+    if (number % 2) { color = colors[1] } else color = colors[0]
+    color = (primeNumber.indexOf(number) >= 0) ? colors[2] : color
+    return color
+  }
+
+  for (let number = 0; number < 32; number++) {
+    let color = numberColor(number, colors)
+    squareList.push(
+      <Square color={color} number={number} />
+    )
+  }
+
+  return (
+    <div classname="container">
+      <div style={{ margin: "auto", width: "640px" }} className="pt-5" >
+        <h3 className="text-center font-weight-bold"> 30 Days Of React</h3>
+        <h5 className="text-center font-weight-light mb-4"> Number Generator</h5>
+        <div className="d-flex flex-row flex-wrap justify-content-center ">
+          {squareList}
+        </div>
+      </div>
     </div>
   )
 }
 
-// countries component
-const Countries = ({ countries }) => {
-  const countryList = countries.map((country) => <Country key={country.name} country={country} />)
-  return <div>{countryList}</div>
-}
-// App component
-const App = () => (
-  <div className='container'>
-    <div>
-      <h3>Countries List</h3>
-      <Countries countries={countries} />
-    </div>
-  </div>
-)
-
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
-
-
-//  ==== Key in mapping arrays ====
-
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
-// const Numbers = ({ numbers }) => {
-//   // modifying array to array of li JSX
-//   const list = numbers.map((num) => <li key={num}>{num}</li>)
-//   return list
-// }
-
-// const App = () => {
-//   const numbers = [1, 2, 3, 4, 5]
-
-//   return (
-//     <div className='container'>
-//       <div>
-//         <h1>Numbers List</h1>
-//         <ul>
-//           <Numbers numbers={numbers} />
-//         </ul>
-//       </div>
-//     </div>
-//   )
-// }
-
-// const rootElement = document.getElementById('root')
-// ReactDOM.render(<App />, rootElement)
