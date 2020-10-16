9b8ecea7004ff1b5e08a4f6b83b6d08360ceec2f
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-16 13:52:51 +0300

    Theory - HOC definition

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import user from './data/userInfo'
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App user={user} />, rootElement)
+const higherOrderComponent = (Component) => {
+  return (props) => {
+    return <Component {...props} />
+  }
+}
