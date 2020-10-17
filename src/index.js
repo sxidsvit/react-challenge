677e9ce5c90cdbdb9ec984d71f5b2e18913a8f55
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-18 02:03:23 +0300

    Theory - Switch & Route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // Home component
-const Home = () => (<h1>Welcome Home</h1>)
+const Home = (props) => <h1>Welcome Home</h1>
 // About component
-const About = () => (<h1>About Us</h1>)
+const About = (props) => <h1>About Us</h1>
 // Contact component
-const Contact = () => (<h1>Contact us</h1>)
+const Contact = (props) => <h1>Contact us</h1>
 // Challenge component
-const Challenges = () => (
+const Challenges = (props) => (
   <div>
     <h1>30 Days Of React Challenge</h1>
   </div>
@@ -19,16 +19,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <h1> Hello !!!</h1>
-        <Route exact path='/' component={Home} />
-        <Route exact strict path='/about' component={About} />
-        <Route exact strict path='/contact' component={Contact} />
-        <Route exact strict path='/challenges' component={Challenges} />
+        <div className='App'>
+          <Switch>
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/challenges' component={Challenges} />
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
       </Router>
     )
   }
 }
 
 const rootElement = document.getElementById('root')
-console.log('rootElement: ', rootElement);
 ReactDOM.render(<App />, rootElement)
