5a9d5029681762bfa4320d3fba3d75ad4271806c
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-18 01:54:42 +0300

    Theory - exsct & strict

--- src/index.js
+++ src/index.js
@@ -1,96 +1,34 @@
-// import React from "react";
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-// import { useRouteMatch, Route } from 'react-router-dom';
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
-} from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
+// Home component
+const Home = () => (<h1>Welcome Home</h1>)
+// About component
+const About = () => (<h1>About Us</h1>)
+// Contact component
+const Contact = () => (<h1>Contact us</h1>)
+// Challenge component
+const Challenges = () => (
+  <div>
+    <h1>30 Days Of React Challenge</h1>
+  </div>
+)
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/topics">
-            <Topics />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <h1> Hello !!!</h1>
+        <Route exact path='/' component={Home} />
+        <Route exact strict path='/about' component={About} />
+        <Route exact strict path='/contact' component={Contact} />
+        <Route exact strict path='/challenges' component={Challenges} />
+      </Router>
+    )
+  }
 }
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Topics() {
-  let match = useRouteMatch();
-  console.log('match: ', match);
-
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
-}
-
 
 const rootElement = document.getElementById('root')
+console.log('rootElement: ', rootElement);
 ReactDOM.render(<App />, rootElement)
