d350e9fc05e6f89f4719a1734339528b6607fa1b
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-18 01:22:37 +0300

    Theory -  Basic Routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React, { Component } from 'react'
+import React from "react";
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className='App'>
-          <h1>React Router DOM</h1>
-        </div>
-      </Router>
-    )
-  }
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 const rootElement = document.getElementById('root')
