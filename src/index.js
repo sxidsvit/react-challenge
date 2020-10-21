ca8423cd421ccd8f8035e0171cdea1f4292fa81a
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-21 16:08:49 +0300

    Exercises: Level 1

--- src/index.js
+++ src/index.js
@@ -10,34 +10,236 @@
 
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-const App = () => {
-  // declaring state
-  const url =
-    'https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg'
+import asabenehImage from './assets/images/asabeneh.jpg'
+import './index.scss'
 
-  const [image, setImage] = useState(url)
+// Fuction to show month date year
+const showDate = (time) => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
 
-  const changeAnimal = () => {
-    let dogURL =
-      'https://static.onecms.io/wp-content/uploads/sites/12/2015/04/dogs-pembroke-welsh-corgi-400x400.jpg'
-    let catURL =
-      'https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg'
-    let result = image === catURL ? dogURL : catURL
-    setImage(result)
-  }
+  const month = months[time.getMonth()].slice(0, 3)
+  const year = time.getFullYear()
+  const date = time.getDate()
+  return ` ${month} ${date}, ${year}`
+}
+
+// User Card Component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
+)
+
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
+
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
+}
+
+const Header = ({ data, styles }) => {
+  const {
+    welcome,
+    title,
+    subtitle,
+    author: { firstName, lastName },
+    date
+  } = data
+
+  console.log('props.styles: ', styles);
+  // console.log('styles: ', styles);
 
   return (
-    <div className='App'>
-      <h1>30 Days Of React</h1>
-      <div className='animal'>
-        <img src={image} alt='animal' />
+    <header style={styles}>
+      <div className='header-wrapper'>
+        <h1>{welcome}</h1>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>
+          {firstName} {lastName}
+        </p>
+        <small>{date}</small>
       </div>
+    </header>
+  )
+}
 
-      <button onClick={changeAnimal} class='btn btn-add'>
-        Change
-      </button>
+const Count = ({ count, addOne, minusOne }) => (
+  <div>
+    <h1>{count} </h1>
+    <div>
+      <Button text='+1' onClick={addOne} style={buttonStyles} />
+      <Button text='-1' onClick={minusOne} style={buttonStyles} />
+    </div>
+  </div>
+)
+
+// TechList Component
+const TechList = (props) => {
+  const { techs } = props
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
+
+// Main Component
+const Main = (props) => {
+  const {
+    techs,
+    user,
+    greetPeople,
+    handleTime,
+    changeBackground,
+    count,
+    addOne,
+    minusOne,
+  } = props
+  return (
+    <main>
+      <div className='main-wrapper' >
+        <p>Prerequisite to get started react.js:</p>
+        <ul>
+          <TechList techs={techs} />
+        </ul>
+        <UserCard user={user} />
+        <Button
+          text='Greet People'
+          onClick={greetPeople}
+          style={buttonStyles}
+        />
+        <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
+        <Button
+          text='Change Background'
+          onClick={changeBackground}
+          style={buttonStyles}
+        />
+        <Count count={count} addOne={addOne} minusOne={minusOne} />
+      </div>
+    </main>
+  )
+}
+
+// Footer Component
+const Footer = ({ date, styles }) => {
+  return (
+    <footer style={styles}>
+      <div className='footer-wrapper'  >
+        <p>Copyright {date.getFullYear()}</p>
+      </div>
+    </footer>
+  )
+}
+
+const App = (props) => {
+  const [count, setCount] = useState(0)
+  const [backgroundColor, setBackgroundColor] = useState({})
+
+  const showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return ` ${month} ${date}, ${year}`
+  }
+  const addOne = () => {
+    setCount(count + 1)
+  }
+
+  // method which subtract one to the state
+  const minusOne = () => {
+    setCount(count - 1)
+  }
+  const handleTime = () => {
+    alert(showDate(new Date()))
+  }
+  const greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+
+  const changeBackground = () => {
+
+    const darkMode = { background: "#000000", color: "#ffffff" }
+    const whiteMode = { background: "#f9f9f9", color: "#000000" }
+    const currentColor = JSON.stringify(backgroundColor) === JSON.stringify(darkMode) ? whiteMode : darkMode
+
+    setBackgroundColor(currentColor)
+  }
+
+  const data = {
+    welcome: 'Welcome to 30 Days Of React',
+    title: 'Getting Started React',
+    subtitle: 'JavaScript Library',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+    date: showDate(new Date()),
+  }
+  const techs = ['HTML', 'CSS', 'JavaScript']
+
+  const user = { ...data.author, image: asabenehImage }
+
+  return (
+    <div className='app' style={backgroundColor} >
+
+      <Header data={data} styles={backgroundColor} />
+      <Main
+        user={user}
+        techs={techs}
+        handleTime={handleTime}
+        greetPeople={greetPeople}
+        changeBackground={changeBackground}
+        addOne={addOne}
+        minusOne={minusOne}
+        count={count}
+      />
+      <Footer date={new Date()} styles={backgroundColor} />
+
     </div>
   )
 }
+
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
