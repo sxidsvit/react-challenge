06351a21320be857133ff76d58c9b997620efa68
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-21 14:30:48 +0300

    Theory - Cat or Dog

--- src/index.js
+++ src/index.js
@@ -10,22 +10,32 @@
 
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
 const App = () => {
-  // Declaring new state variable
-  const [count, setCount] = useState(0)
-  const addOne = () => {
-    let value = count + 1
-    setCount(value)
+  // declaring state
+  const url =
+    'https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg'
+
+  const [image, setImage] = useState(url)
+
+  const changeAnimal = () => {
+    let dogURL =
+      'https://static.onecms.io/wp-content/uploads/sites/12/2015/04/dogs-pembroke-welsh-corgi-400x400.jpg'
+    let catURL =
+      'https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg'
+    let result = image === catURL ? dogURL : catURL
+    setImage(result)
   }
-  const minusOne = () => {
-    let value = count - 1
-    setCount(value)
-  }
+
   return (
     <div className='App'>
-      <h1>{count} </h1>
-      <button onClick={addOne}>Add One</button> <button onClick={minusOne}>Minus One</button>
+      <h1>30 Days Of React</h1>
+      <div className='animal'>
+        <img src={image} alt='animal' />
+      </div>
+
+      <button onClick={changeAnimal} class='btn btn-add'>
+        Change
+      </button>
     </div>
   )
 }
