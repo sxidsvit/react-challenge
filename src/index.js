4f096b49a006f6ea105f5ac76232bb6597871288
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-21 14:28:55 +0300

    Theory -separate function with kook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 /* eslint-disable space-before-function-paren */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
 
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(<App />, rootElement)
+
+// ================================================
+
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // Declaring new state variable
+  const [count, setCount] = useState(0)
+  const addOne = () => {
+    let value = count + 1
+    setCount(value)
+  }
+  const minusOne = () => {
+    let value = count - 1
+    setCount(value)
+  }
+  return (
+    <div className='App'>
+      <h1>{count} </h1>
+      <button onClick={addOne}>Add One</button> <button onClick={minusOne}>Minus One</button>
+    </div>
+  )
+}
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
