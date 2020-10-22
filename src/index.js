871f4e020d6147db8fa7bc15e2b633e47b4c90b1
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-22 15:38:39 +0300

    Theory - Getting multiple input data from form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,87 @@
 
-import React from 'react'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+// import user from './data/userInfo'
+
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(<App user={user} />, rootElement)
+
+// ========================================================
+
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import user from './data/userInfo'
+
+const App = (props) => {
+  const initialState = {
+    firstName: '',
+    lastName: '',
+    country: '',
+    title: '',
+  }
+  const [formData, setData] = useState(initialState)
+
+  const onChange = (e) => {
+    const { name, value } = e.target
+    setData({ ...formData, [name]: value })
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+    /*
+     the is the place where we connect backend api 
+     to send the data to the database
+     */
+    console.log(formData)
+  }
+
+  // accessing the state value by destrutcturing the state
+  const { firstName, lastName, title, country } = formData
+  return (
+    <div className='App'>
+      <h3>Add Student</h3>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type='text'
+            name='firstName'
+            placeholder='First Name'
+            value={firstName}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <input
+            type='text'
+            name='lastName'
+            placeholder='Last Name'
+            value={lastName}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <input
+            type='text'
+            name='country'
+            placeholder='Country'
+            value={country}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <input
+            type='text'
+            name='title'
+            placeholder='Title'
+            value={title}
+            onChange={onChange}
+          />
+        </div>
+
+        <button class='btn btn-success'>Submit</button>
+      </form>
+    </div>
+  )
+}
 
 const rootElement = document.getElementById('root')
-ReactDOM.render(<App user={user} />, rootElement)
+ReactDOM.render(<App />, rootElement)
