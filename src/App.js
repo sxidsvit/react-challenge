c1ced99c83c7114e5f41b710a64c33748fd81b78
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-27 02:45:20 +0200

    Exercises - initial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React, { useRef } from 'react'
 
-const App = (props) => {
-  const ref = useRef(null)
-  const onClick = () => {
-    ref.current.style.backgroundColor = '#61dbfb'
-    ref.current.style.padding = '50px'
-    ref.current.style.textAlign = 'center'
+const hexaColor = () => {
+  let str = '0123456789abcdef'
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length)
+    color += str[index]
   }
+  return '#' + color
+}
+
+const HexaColor = ({ hexaColor }) => {
+
+  const bgcolor = hexaColor()
+
+  const styles = {
+    hexaColor: {
+      width: "120px",
+      height: "120px",
+      backgroundColor: bgcolor,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+      border: "2px solid #fff",
+      color: "#fff",
+      fontWeight: "500",
+      fontSize: "18px",
+    }
+  }
+
   return (
-    <div className='App'>
-      <h1 ref={ref}>How to style HTML from the DOM tree using useRef</h1>
-      <button onClick={onClick}>Style it</button>
+    <div style={styles.hexaColor}>
+      { bgcolor}
+    </div>
+  )
+}
+
+const App = () => {
+
+  const hexadecimalList = []
+  for (let number = 0; number < 32; number++) {
+    hexadecimalList.push(<HexaColor key={number + 1} hexaColor={hexaColor} />)
+  }
+
+  return (
+    <div className="container">
+      <div style={{ margin: "auto", width: "960px" }} className="pt-5 mb-5" >
+        <h3 className="text-center font-weight-bold"> 30 Days Of React</h3>
+        <h5 className="text-center font-weight-light mb-4">Hexadecimal Colors</h5>
+        <div className="d-flex flex-row flex-wrap justify-content-center ">
+          {hexadecimalList}
+        </div>
+      </div>
     </div>
   )
 }
