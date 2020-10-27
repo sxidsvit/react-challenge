144451e1555b9c351a2bff67714d5b8c5ba07ec6
sxidsvit <s.p.antonyuk@gmail.com>
2020-10-27 02:20:46 +0200

    Theory - Accessing and Styling a DOM element

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React, { useRef } from 'react'
 const App = (props) => {
   const ref = useRef(null)
   const onClick = () => {
-    let content = ref.current.textContent
-    alert(content)
-    console.log(content)
+    ref.current.style.backgroundColor = '#61dbfb'
+    ref.current.style.padding = '50px'
+    ref.current.style.textAlign = 'center'
   }
   return (
     <div className='App'>
-      <h1 ref={ref}>How to getting content from the DOM tree</h1>
-      <button onClick={onClick}>Getting Content</button>
+      <h1 ref={ref}>How to style HTML from the DOM tree using useRef</h1>
+      <button onClick={onClick}>Style it</button>
     </div>
   )
 }
